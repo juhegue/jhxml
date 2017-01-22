94a760238202dda9de97f97a99223e67b5e02f9e
Juan Hevilla <j.hevilla@diagram.es>
2017-01-22 23:42:44 +0100

    .

--- constantes.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#--generales
-NVERSION = "0.1"
-CREADO = '31/12/2016'
-APLICACION = 'jhxml'
-AUTOR = 'Juan Hevilla Guerrero'
-AUTOR_EMAIL = 'juhegue@gmail.es'
-WEB = 'https://jhxlm.sourceforge.io'
